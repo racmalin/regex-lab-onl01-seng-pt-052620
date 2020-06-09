4201d2fdba6917d12e5e3f8ee1f366fa7f5122c6
Kevin C Malin <racmalin@yahoo.com>
2020-06-09 02:03:08 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,20 +1,22 @@
 def starts_with_a_vowel?(word)
- 
-
+word.match(/\A[aeiouAEIOU]/) != nil
 end
+
+starts_with_a_vowel?("!ord")
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+text.scan(/un\w*ing/)
 end
+
+words_starting_with_un_and_ending_with_ing("unassuming ambiguous understanding pomp circumstance uninteresting uncompromising grouchy corollary")
 
 def words_five_letters_long(text)
-
+text.scan(/\b\w{5}\b/)
 end
+
+
+words_five_letters_long("extreme briny crepe parking snaps grouping snafu round dog be fork spoon")
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
-end
-
-def valid_phone_number?(phone)
-
+text.match(/\A[A-Z]\w*\W\b/) != nil
 end
