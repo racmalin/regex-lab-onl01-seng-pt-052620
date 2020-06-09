ff1124004835a81358575fd808a64208bd8b0a0e
Kevin C Malin <racmalin@yahoo.com>
2020-06-09 02:08:03 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -21,7 +21,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 text.match(/\A[A-Z]\w*\W\b/) != nil
 end
 
-def first_word_capitalized_and_ends_with_punctuation?("I stepped on a Corn Flake, now I'm a cereal killer.")
+first_word_capitalized_and_ends_with_punctuation?("I stepped on a Corn Flake, now I'm a cereal killer.")
 end
 
 
